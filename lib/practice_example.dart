8800e5d7a35420296c672cf390f6969a2d3b586a
pruthvikumar <pruthvikumar247@gmail.com>
2023-11-13 01:05:48 +0530

    hero section - student image fix

---
+++ lib/practice_example.dart
@@ -0,0 +1,6 @@
+extension EmailValidator on String {
+  bool isValidEmail() {
+    final RegExp emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    return emailRegex.hasMatch(this);
+  }
+}
---
+++ website/documents/content.md
@@ -0,0 +1,3 @@
+# Designing better tomorrow together
+
+### A School with distinctive character. We focus on adopting the system to each child, a rarity in th days of templatised education.
